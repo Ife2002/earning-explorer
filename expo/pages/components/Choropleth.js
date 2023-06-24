dff8f215c9b0cc3379afba9311f597330280c880
Ife Asiadiachi <iasiadiachi@gmail.com>
2023-06-24 03:22:59 +0000

    commented them out

--- expo/pages/components/Choropleth.js
+++ expo/pages/components/Choropleth.js
@@ -1,43 +1,43 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
 
 
-const ChoroplethMap = () => {
-  const [geojsonData, setGeojsonData] = useState(null);
+// const ChoroplethMap = () => {
+//   const [geojsonData, setGeojsonData] = useState(null);
 
-  useEffect(() => {
-    // Load GeoJSON data
-    fetch('/path/to/your/geojson/data.json')
-      .then((response) => response.json())
-      .then((data) => {
-        setGeojsonData(data);
-      })
-      .catch((error) => {
-        console.error('Error loading GeoJSON data:', error);
-      });
-  }, []);
+//   useEffect(() => {
+//     // Load GeoJSON data
+//     fetch('/path/to/your/geojson/data.json')
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setGeojsonData(data);
+//       })
+//       .catch((error) => {
+//         console.error('Error loading GeoJSON data:', error);
+//       });
+//   }, []);
 
-  return (
-    <MapContainer
-      center={[0, 0]}
-      zoom={2}
-      style={{ height: '400px', width: '100%' }}
-    >
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+//   return (
+//     <MapContainer
+//       center={[0, 0]}
+//       zoom={2}
+//       style={{ height: '400px', width: '100%' }}
+//     >
+//       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-      {geojsonData && (
-        <GeoJSON
-          data={geojsonData}
-          style={(feature) => ({
-            fillColor: getColor(feature.properties.value),
-            weight: 1,
-            color: 'white',
-            fillOpacity: 0.7,
-          })}
-        />
-      )}
-    </MapContainer>
-  );
-};
+//       {geojsonData && (
+//         <GeoJSON
+//           data={geojsonData}
+//           style={(feature) => ({
+//             fillColor: getColor(feature.properties.value),
+//             weight: 1,
+//             color: 'white',
+//             fillOpacity: 0.7,
+//           })}
+//         />
+//       )}
+//     </MapContainer>
+//   );
+// };
 
-export default ChoroplethMap;
+// export default ChoroplethMap;
 
